da0aae7f317674818af8dffcde3eda096f3a2441
Russel <154447088+HeavenlyRussel@users.noreply.github.com>
2025-02-01 09:03:53 +0800

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,24 @@
+from os import system
+
+system("cls")
+
+print("Hello and welcome to my program where even if you exit this program, your data is stored")
+print("\n")
+
+file = open("Practice Data Storage.txt", 'r')
+lastData = file.read()
+
+if lastData == "":
+    pass
+else:
+    print(f"Last data entered is {lastData}")
+    
+file.close()
+
+print("\n")
+print("Now enter a string for example: ", end = "")
+exampleNumber = input()
+
+file = open("Practice Data Storage.txt", 'w')
+file.write(exampleNumber)
+file.close()
